cb71eac8b56433cd723367e3bb993c8dcee2599b
Rafal Gumienny <r.gumienny@unibas.ch>
2016-06-20 17:07:51 +0200

    conf.py andjustment

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ class Mock(MagicMock):
 MOCK_MODULES = ['biopython', 'HTSeq', 'drmaa', 'drmaa.errors', 'numpy', 'pandas',
                 'mysql-python', 'statsmodels', 'statsmodels.api', 'Jobber',
                 'ushuffle', 'pylab', 'seaborn', 'scipy', 'modules', 'modules.snoRNA',
-                'matplotlib', 'matplotlib.gridspec', 'scipy.stats', 'pybedtools', 'bx.bbi.bigwig_file', 'MetaProfile']
+                'matplotlib', 'matplotlib.gridspec', 'scipy.stats', 'pybedtools', 'bx.bbi.bigwig_file', 'MetaProfile', 'bx.intervals.intersection']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
