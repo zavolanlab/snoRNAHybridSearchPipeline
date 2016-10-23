59a227d191f8db2f7f1d262c3bcd3f07b4dc36e1
Rafal Gumienny <r.gumienny@unibas.ch>
2016-10-23 16:28:35 +0200

    Cleaning up repo

--- modules/MetaProfile/MetaProfile/__init__.py
+++
@@ -1,5 +0,0 @@
-from Signal import Signal, get_signals, BedWrapper
-from Profile import Profile
-from Window import Window
-from MetaProfiler import MetaProfiler, get_profiler
-import utils as utils
--- modules/MetaProfile/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-========
-Usage
-========
-
-To use MetaProfile in a project::
-
-    import MetaProfile
